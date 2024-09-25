8eb9c76e84fe8811363369c4bf983efa1b84dfe8
mannamsarath <mannamsarath58@gmail.com>
2024-09-25 12:34:29 +0530

    changed health check as part of client requirement

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
     var stat = {
-        app: 'OK',
+        app: 'OKk',
         mongo: mongoConnected
     };
     res.json(stat);
